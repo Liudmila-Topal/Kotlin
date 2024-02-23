7bf2f58a3b5744de8fedc99b5c9ab256addac4d3
Liudmila-Topal <prepelitsa.liudmila@gmail.com>
2024-02-23 18:54:16 +0200

    Comments

---
+++ src/main/kotlin/CommentExamples.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    // Single line comment
+    println("Hello World!")
+
+    println("Program arguments: ${args.joinToString()}") // Single line comment
+
+    /* One line comment */ println("Hello World!")
+
+    println/* One line comment */("Hello World!")
+
+    /*
+    Multi
+    lines
+    comment
+     */
+}
